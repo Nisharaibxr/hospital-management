192be08507308c19593bcce45c42d623137b45f1
Atul kumar <85097109+Nisharaibxr@users.noreply.github.com>
2022-11-15 22:58:48 +0530

    Website to manage hospital

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD4Xwz4foE92YcRWknFrEU2Z2wVui8lOM8",
+    authDomain: "hospital-management-66fb6.firebaseapp.com",
+    projectId: "hospital-management-66fb6",
+    storageBucket: "hospital-management-66fb6.appspot.com",
+    messagingSenderId: "81998493859",
+    appId: "1:81998493859:web:e5f30dc1c4a030aaec2716"
+  }
+};
